a8319d08fbaf8de1423c1eacd2b3544cc67b87c4
idylleapp01 <idylleapp01@gmail.com>
2026-07-10 20:39:06 +0100

    Switch runtime implementation from bcrypt to bcryptjs for render cross-compilation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Updated to bcryptjs for absolute platform stability
 
 const app = express();
 const PORT = process.env.PORT || 10000;
